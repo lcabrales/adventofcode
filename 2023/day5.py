a4e9b47225df979c02c8649b1b037166a3ac5c07
lcabrales <lcabrales@hastexp.com>
2023-12-07 11:47:39 +0000

    Day 5 update

--- 2023/day5.py
+++ 2023/day5.py
@@ -1,3 +1,6 @@
+from timeit import default_timer as timer
+from datetime import timedelta
+
 file_path = 'day5.txt'
 
 with open(file_path, 'r') as file:
@@ -7,45 +10,57 @@ sections = lines.split('\n\n')
 seeds = [int(x) for x in sections[0].split('seeds:')[1].strip().split()]
 others = sections[1:]
 
-def part1():
+def process_seed(seed, current_ans):
+    current = seed
+    for map_str in others:
+        map_lines = [[int(y) for y in x.split()] for x in map_str.split(':')[1].strip().split('\n')]
+        for map_line in map_lines:
+            destination_s, source_s, len = map_line
+            if source_s <= current <= source_s + len:
+                current = current - source_s + destination_s
+                break
+    
+    return min(current_ans, current)
+
+def part1(seeds):
     ans = max(seeds)
     for seed in seeds:
-        current = seed
-        for map_str in others:
-            map_lines = [[int(y) for y in x.split()] for x in map_str.split(':')[1].strip().split('\n')]
-            for map_line in map_lines:
-                destination_s, source_s, len = map_line
-                if source_s <= current <= source_s + len:
-                    current = current - source_s + destination_s
-                    break
-                    
-        ans = min(ans, current)
+        ans = process_seed(seed, ans)
 
-    print(ans) # part 1
+    return ans
 
 
 def part2():
     seeds_pairs = list(zip(seeds[::2], seeds[1::2]))
-    seeds_dict = {}
+    ans = float('inf')
     for seed_s, seed_l in seeds_pairs:
-        seeds_dict[seed_s] = seed_l
+        seed_s = int(seed_s)
+        seed_l = int(seed_l)
+        
+        # improve performance?
+        div_by = 100
+        ranges = []
+        for div in range(div_by):
+            f1 = seed_l // div_by * div
+            if div == div_by - 1:
+                f2 = seed_l
+            else:
+                f2 = seed_l // div_by * (div + 1)
+            ranges.append(range(f1, f2))
 
-    ans = max(seeds_dict, key=lambda k: seeds_dict[k])
-    for seed_s in seeds_dict.keys():
-        len = seeds_dict[seed_s]
-        for seed in range(seed_s, seed_s + len):
-            current = seed
-            for map_str in others:
-                map_parts = map_str.split(':')
-                map_lines = [[int(y) for y in x.split()] for x in map_parts[1].strip().split('\n')]
-                
-                for destination_s, source_s, length in map_lines:
-                    if source_s <= current <= source_s + length:
-                        current = current - source_s + destination_s
-                        break
-                        
-            ans = min(ans, current)
-    print(ans) # part 2
+        print(ranges)
+        for range_val in ranges:
+            start = timer()
+            for val in range_val:
+                seed = val + seed_s
 
-#part1()
-part2()
+                if val % 1_000_000 == 0:
+                    end = timer()
+                    print(f"trying seed: {seed} - {timedelta(seconds=end-start)}")
+                    start = timer()
+
+                ans = process_seed(seed, ans)
+    return ans
+
+print(part1(seeds))
+print(part2())
