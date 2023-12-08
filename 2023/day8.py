523c2b1ea98315ca7fc466f5679c31055664d394
lcabrales <lcabrales@hastexp.com>
2023-12-08 11:53:14 +0000

    Day 8

---
+++ 2023/day8.py
@@ -0,0 +1,51 @@
+import math
+
+file_path = 'day8.txt'
+
+with open(file_path, 'r') as file:
+    lines = file.read()
+
+inst, nodes_str = lines.split('\n\n')
+inst = list(inst)
+
+nodes = {}
+for node_str in nodes_str.split('\n'):
+    root, pointers = node_str.split('=')
+    left, right = pointers.split(',')
+    root = root.strip()
+    left = left.replace('(', '').strip()
+    right = right.replace(')', '').strip()
+    nodes[root] = (left, right)
+
+
+def solve_key(key, is_part1 = False):
+    current = nodes[key]
+    ans = 0
+    while True:
+        dir = inst[ans % len(inst)] # overflow
+        ans += 1
+
+        dest = current[0 if dir == 'L' else 1]
+        if is_part1:
+            if dest == 'ZZZ':
+                return ans
+        else:
+            if dest.endswith('Z'):
+                return ans
+        current = nodes[dest]
+
+
+def part1():
+    print(solve_key('AAA', True)) # answer
+
+
+def part2():
+    current_keys = [key for key in nodes if key.endswith('A')]
+    steps_l = []
+    for key in current_keys:
+        steps_l.append(solve_key(key))
+    print(math.lcm(*steps_l)) # answer
+
+
+part1()
+part2()
